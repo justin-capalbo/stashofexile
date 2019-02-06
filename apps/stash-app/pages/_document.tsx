4cdb3caf145897509c8392d738352cc6f7b2e812
Justin Capalbo <capalbo.justin@gmail.com>
2019-02-06 10:38:44 -0500

    Add back semantic UI CSS

--- apps/stash-app/pages/_document.tsx
+++ apps/stash-app/pages/_document.tsx
@@ -17,6 +17,7 @@ export default class MyDocument extends Document<Props> {
         return (
             <html>
                 <Head>
+                    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css" />
                     <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
                 </Head>
                 <body>
